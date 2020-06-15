c06277d98b2965da0d6ef78544d2dd317cdc4b73
akshay454 <akshayrai121@gmail.com>
2020-06-15 16:06:54 +0000

    add MongoDB Atlas

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var commentRoutes = require("./routes/comments"),
 	indexRoutes = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://akshayrai:heisenberg121@cluster0-9z9v4.mongodb.net/yelpcamp?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+
 app.set("view engine","ejs");
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({extended : true})); // to support URL-encoded bodies
